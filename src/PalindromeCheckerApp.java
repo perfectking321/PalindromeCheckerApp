0d63a7db18117830e0f45266fb2334dfd416a2c6
perfectking321 <perfectkings321@gmail.com>
2026-02-23 09:56:58 +0530

    use case 5

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,20 +1,20 @@
+import java.util.Stack;
+
 public class PalindromeCheckerApp {
     public static void main(String[] args){
         String text = "madam";
-        char[] chars = text.toCharArray();
-        int start=0,end=chars.length-1;
+        Stack<Character> st = new Stack<>();
+        for(char c: text.toCharArray()){
+            st.push(c);
+        }
         boolean isPalin = true;
-        while(start<end){
-            if(chars[start]==chars[end]){
-                start++;
-                end--;
-            }
-            else {
-                isPalin = false;
+        for(char c:text.toCharArray()){
+            if(c!=st.pop()){
+                isPalin=false;
                 break;
             }
         }
-        System.out.println(text+" is palindrome: "+isPalin);
+        System.out.print(text+"is a palindrome?:"+isPalin);
     }
 }
 
