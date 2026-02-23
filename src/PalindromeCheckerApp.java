ce46eb6a77eb95d99f50200eb51fc5fbbc8f66ae
perfectking321 <perfectkings321@gmail.com>
2026-02-23 10:04:48 +0530

    Use case 6

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,20 +1,23 @@
-import java.util.Stack;
+import java.util.LinkedList;
+import java.util.*;
 
 public class PalindromeCheckerApp {
     public static void main(String[] args){
-        String text = "madam";
+        String text = "civic";
         Stack<Character> st = new Stack<>();
+        Queue<Character> q = new LinkedList<>();
         for(char c: text.toCharArray()){
             st.push(c);
+            q.add(c);
         }
         boolean isPalin = true;
         for(char c:text.toCharArray()){
-            if(c!=st.pop()){
+            if(q.remove()!=st.pop()){
                 isPalin=false;
                 break;
             }
         }
-        System.out.print(text+"is a palindrome?:"+isPalin);
+        System.out.print(text+" is a palindrome?:"+isPalin);
     }
 }
 
